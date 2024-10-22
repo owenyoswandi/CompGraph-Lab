ce2813c46ec6a275dd9bf8975a4881c6ec2eebbe
Josiah Owen Yoswandi <josiah.yoswandi@gmail.com>
2024-10-22 21:19:44 +0700

    +mercury, venus, earth

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ import { OrbitControls } from './ThreeJS/examples/jsm/controls/OrbitControls.js'
 
 let scene, camera, w, h, rndr, control, camera2, selectedCamera
 
-const init = () => {
+let init = () => {
     scene = new THREE.Scene()
 
     w = window.innerWidth
@@ -14,7 +14,7 @@ const init = () => {
     camera.lookAt(640, 320, 0)
 
     camera2 = new THREE.PerspectiveCamera(90, w/h, 0.1, 10000)
-    camera2.position.set(0, 0, 0)
+    camera2.position.set(640, 480, 240)
     camera2.lookAt(640, 320, 0)
 
     selectedCamera = camera
@@ -23,6 +23,8 @@ const init = () => {
     rndr.setSize(w, h)
     document.body.appendChild(rndr.domElement)
 
+    rndr.setClearColor(0xffffff)
+
     rndr.shadowMap.enabled = true 
     rndr.shadowMap.type = THREE.PCFShadowMap
 
@@ -30,6 +32,21 @@ const init = () => {
 
     let sun = createSun()
     sun.position.set(640, 320, 0)
+
+    let mercury = createMercury()
+    mercury.position.set(58, 320, 0)
+    mercury.castShadow = true
+    mercury.receiveShadow = true
+
+    let venus = createVenus()
+    venus.position.set(80, 320, 0)
+    venus.castShadow = true
+    venus.receiveShadow = true
+
+    let earth = createEarth()
+    earth.position.set(100, 320, 0)
+    earth.castShadow = true
+    earth.receiveShadow = true
     
     let point = createPointLight()
     point.position.set(640, 320, 0)
@@ -37,23 +54,24 @@ const init = () => {
 
     let spot = createSpotlight()
 
-    scene.add(sun, point)
+    scene.add(sun, mercury, venus, earth)
+    scene.add(point)
 }
 
-const createPointLight = () =>{
+let createPointLight = () =>{
     let lampu = new THREE.PointLight(0xffffff, 1, 1280)
     // let helpLampu = new THREE.PointLightHelper(lampu, 0xffffff)
 
     return lampu
 }
 
-const createSpotlight = () =>{
+let createSpotlight = () =>{
     let lampu = new THREE.SpotLight(0xffffff, 8, 8)
 
     return lampu
 }
 
-const createSun = () =>{
+let createSun = () =>{
     let geo = new THREE.SphereGeometry(40, 64, 64)
 
     let textureLoad = new THREE.TextureLoader()
@@ -73,9 +91,70 @@ const createSun = () =>{
     return new THREE.Mesh(geo, mats)
 }
 
-const render = () => {
+let createMercury = () => {
+    let geo = new THREE.SphereGeometry(3.2, 64, 64)
+
+    let textureLoad = new THREE.TextureLoader()
+    let texture  = textureLoad.load(
+        "./assets/textures/mercury.jpg"
+    )
+
+    texture.wrapS = THREE.RepeatWrapping
+    texture.wrapT = THREE.RepeatWrapping
+
+    texture.repeat.set(1, 1)
+
+    let mats = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        map: texture
+    })
+    return new THREE.Mesh(geo, mats)
+}
+
+let createVenus = () => {
+    let geo = new THREE.SphereGeometry(4.8, 64, 64)
+
+    let textureLoad = new THREE.TextureLoader()
+    let texture  = textureLoad.load(
+        "./assets/textures/venus.jpg"
+    )
+
+    texture.wrapS = THREE.RepeatWrapping
+    texture.wrapT = THREE.RepeatWrapping
+
+    texture.repeat.set(1, 1)
+
+    let mats = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        map: texture
+    })
+    return new THREE.Mesh(geo, mats)
+}
+
+let createEarth = () => {
+    let geo = new THREE.SphereGeometry(4.8, 64, 64)
+
+    let textureLoad = new THREE.TextureLoader()
+    let texture  = textureLoad.load(
+        "./assets/textures/earth.jpg"
+    )
+
+    texture.wrapS = THREE.RepeatWrapping
+    texture.wrapT = THREE.RepeatWrapping
+
+    texture.repeat.set(1, 1)
+
+    let mats = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        map: texture
+    })
+    return new THREE.Mesh(geo, mats)
+}
+
+
+let render = () => {
     requestAnimationFrame(render)
-    rndr.render(scene, camera)
+    rndr.render(scene, selectedCamera)
     control.update()
 }
 
@@ -83,7 +162,7 @@ window.onresize = () => {
     w = window.innerWidth
     h = window.innerHeight
 
-    camera.aspect = w/h
+    camera = w/h
     rndr.setSize(w, h)
     camera.updateProjectionMatrix()
 }
