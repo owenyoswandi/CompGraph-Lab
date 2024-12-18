7e8c15610a7a56881f56769f06177c6cb5405fbd
Josiah Owen Yoswandi <josiah.yoswandi@gmail.com>
2024-12-18 20:15:22 +0700

    fixed and compacted

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import { OrbitControls } from './ThreeJS/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from './ThreeJS/examples/jsm/loaders/GLTFLoader.js'
 
 let scene, camera, w, h, rndr, control, camera2, selectedCamera, spot, model
+let mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, satellite
 
 function init () {
     scene = new THREE.Scene()
@@ -11,12 +12,7 @@ function init () {
     h = window.innerHeight
 
     camera = new THREE.PerspectiveCamera(75, w/h, 0.1, 10000)
-    camera.position.set(640, 480, 240)
-    camera.lookAt(640, 320, 0)
-
     camera2 = new THREE.PerspectiveCamera(90, w/h, 0.1, 10000)
-    camera2.position.set(640, 480, 240)
-    camera2.lookAt(640, 320, 0)
 
     selectedCamera = camera
 
@@ -31,56 +27,20 @@ function init () {
 
     control = new OrbitControls(camera, rndr.domElement)
 
+    updateCameraPosition()
+
     let sun = createSun()
-    sun.position.set(640, 320, 0)
-
-    let mercury = createMercury()
-    mercury.position.set(58, 320, 0)
-    mercury.castShadow = true
-    mercury.receiveShadow = true
-
-    let venus = createVenus()
-    venus.position.set(80, 320, 0)
-    venus.castShadow = true
-    venus.receiveShadow = true
-
-    let earth = createEarth()
-    earth.position.set(100, 320, 0)
-    earth.castShadow = true
-    earth.receiveShadow = true
-
-    let mars = createMars()
-    mars.position.set(130, 320, 0)
-    mars.castShadow = true
-    mars.receiveShadow = true
-
-    let jupiter = createJupiter()
-    jupiter.position.set(175, 320, 0)
-    jupiter.castShadow = true
-    jupiter.receiveShadow = true
-
-    let saturn = createSaturn()
-
-    let uranus = createUranus()
-
-    let neptune = createNeptune()
-    neptune.position.set(320, 320, 0)
-    neptune.castShadow = true
-    neptune.receiveShadow = true
-    
     let point = createPointLight()
-    point.position.set(640, 320, 0)
-    point.castShadow = true
+    let sunGroup = new THREE.Group()
+    sunGroup.add(sun, point)
+    sunGroup.position.set(640, 320, 0)
 
-    spot = new THREE.SpotLight(0xffffff, 8, 8)
     let satellite = createSatellite()
-    satellite.position.set(100 + 8, 320, 0);
-    satellite.castShadow = false;
-    satellite.receiveShadow = true;
 
-    scene.add(sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, satellite)
-    scene.add(point, spot)
+    createPlanets()
 
+    scene.add(sunGroup, satellite)
+    
     let loader = new GLTFLoader()
     loader.load("./assets/model/spaceship/scene.gltf", function ( gltf ) {
         model = gltf.scene
@@ -94,8 +54,130 @@ function init () {
     });
     loadSkyBox()
 }
+
+function createPlanet({ name, size, position, texturePath, ring = null }) {
+    const geo = new THREE.SphereGeometry(size, 64, 64);
+
+    const textureLoad = new THREE.TextureLoader();
+    const texture = textureLoad.load(texturePath);
+
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(1, 1);
+
+    const mats = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        map: texture
+    });
+
+    const planet = new THREE.Mesh(geo, mats);
+    planet.castShadow = true;
+    planet.receiveShadow = true;
+
+    const planetGroup = new THREE.Group();
+    planetGroup.add(planet);
+    planetGroup.position.set(...position);
+
+    if (ring) {
+        const { innerRadius, outerRadius, ringTexturePath, rotationX } = ring;
+        const ringGeo = new THREE.RingGeometry(innerRadius, outerRadius, 64);
+
+        const ringTexture = textureLoad.load(ringTexturePath);
+        ringTexture.wrapS = THREE.RepeatWrapping;
+        ringTexture.wrapT = THREE.RepeatWrapping;
+        ringTexture.repeat.set(1, 1);
+
+        const ringMats = new THREE.MeshStandardMaterial({
+            color: 0xffffff,
+            map: ringTexture,
+            side: THREE.DoubleSide
+        });
+
+        const ringMesh = new THREE.Mesh(ringGeo, ringMats);
+        ringMesh.castShadow = false;
+        ringMesh.receiveShadow = true;
+        ringMesh.rotation.x = rotationX;
+
+        planetGroup.add(ringMesh);
+    }
+
+    return planetGroup;
+}
+
+function createPlanets() {
+    mercury = createPlanet({
+        name: "Mercury",
+        size: 3.2,
+        position: [58, 320, 0],
+        texturePath: "./assets/textures/mercury.jpg"
+    });
+
+    venus = createPlanet({
+        name: "Venus",
+        size: 4.8,
+        position: [80, 320, 0],
+        texturePath: "./assets/textures/venus.jpg"
+    });
+
+    earth = createPlanet({
+        name: "Earth",
+        size: 4.8,
+        position: [100, 320, 0],
+        texturePath: "./assets/textures/earth.jpg"
+    });
+
+    mars = createPlanet({
+        name: "Mars",
+        size: 4,
+        position: [130, 320, 0],
+        texturePath: "./assets/textures/mars.jpg"
+    });
+
+    jupiter = createPlanet({
+        name: "Jupiter",
+        size: 13,
+        position: [175, 320, 0],
+        texturePath: "./assets/textures/jupiter.jpg"
+    });
+
+    saturn = createPlanet({
+        name: "Saturn",
+        size: 10,
+        position: [240, 320, 0],
+        texturePath: "./assets/textures/saturn.jpg",
+        ring: {
+            innerRadius: 16,
+            outerRadius: 32,
+            ringTexturePath: "./assets/textures/saturn_ring.png",
+            rotationX: Math.PI / 4
+        }
+    });
+
+    uranus = createPlanet({
+        name: "Uranus",
+        size: 13,
+        position: [280, 320, 0],
+        texturePath: "./assets/textures/uranus.jpg",
+        ring: {
+            innerRadius: 16,
+            outerRadius: 20,
+            ringTexturePath: "./assets/textures/uranus_ring.png",
+            rotationX: -Math.PI / 4
+        }
+    });
+
+    neptune = createPlanet({
+        name: "Neptune",
+        size: 13,
+        position: [320, 320, 0],
+        texturePath: "./assets/textures/neptune.jpg"
+    });
+
+    scene.add(mercury, venus, earth, mars, jupiter, saturn, uranus, neptune);
+}
+
 function loadSkyBox() {
-    let CubeTexture = new THREE.TextureLoader
+    let CubeTexture = new THREE.TextureLoader()
     let geo = new THREE.BoxGeometry(4260, 4260, 4260)
     let skyBoxMat = [
         new THREE.MeshBasicMaterial({map: CubeTexture.load("./assets/skybox/right.png"),
@@ -123,6 +205,7 @@ function loadSkyBox() {
 
 function createPointLight (){
     let lampu = new THREE.PointLight(0xffffff, 1, 1280)
+    lampu.castShadow = true
     return lampu
 }
 
@@ -133,7 +216,12 @@ function updateSpotlightPosition(spaceship) {
             spaceship.position.y + 6, 
             spaceship.position.z
         );
-        
+        spot.target.position.set(
+            spaceship.position.x,
+            spaceship.position.y,
+            spaceship.position.z
+        );
+        spot.target.updateMatrixWorld();
     }
 }
 function updateCamera2Position(spaceship) {
@@ -171,222 +259,6 @@ function createSun () {
     return new THREE.Mesh(geo, mats)
 }
 
-function createMercury () {
-    let geo = new THREE.SphereGeometry(3.2, 64, 64)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/mercury.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    return new THREE.Mesh(geo, mats)
-}
-
-function createVenus () {
-    let geo = new THREE.SphereGeometry(4.8, 64, 64)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/venus.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    return new THREE.Mesh(geo, mats)
-}
-
-function createEarth () {
-    let geo = new THREE.SphereGeometry(4.8, 64, 64)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/earth.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    return new THREE.Mesh(geo, mats)
-}
-
-function createMars (){
-    let geo = new THREE.SphereGeometry(4)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/mars.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    return new THREE.Mesh(geo, mats)
-}
-
-function createJupiter (){
-    let geo = new THREE.SphereGeometry(13)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/jupiter.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    return new THREE.Mesh(geo, mats)
-}
-
-function createSaturn (){
-    let geo = new THREE.SphereGeometry(10)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/saturn.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    let saturn = new THREE.Mesh(geo, mats)
-    saturn.castShadow = true
-    saturn.receiveShadow = true
-
-    let ringGeo = new THREE.RingGeometry(16, 32, 64)
-    let ringTextureLoad = new THREE.TextureLoader()
-    let ringTexture  = ringTextureLoad.load(
-        "./assets/textures/saturn_ring.png"
-    )
-
-    ringTexture.wrapS = THREE.RepeatWrapping
-    ringTexture.wrapT = THREE.RepeatWrapping
-
-    ringTexture.repeat.set(1, 1)
-    let ringMats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture,
-        side: THREE.DoubleSide
-    })
-    let ring= new THREE.Mesh(ringGeo, ringMats)
-    ring.castShadow = false
-    ring.receiveShadow = true
-    ring.rotation.x = Math.PI/4
-
-    let saturnGroup = new THREE.Group()
-    saturnGroup.add(saturn)
-    saturnGroup.add(ring)
-    saturnGroup.position.set(240, 320, 0); // Set Saturn's position as a group
-    return saturnGroup;
-}
-
-function createUranus (){
-    let geo = new THREE.SphereGeometry(13)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/uranus.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    let uranus =  new THREE.Mesh(geo, mats)
-    uranus.castShadow = true
-    uranus.receiveShadow = true
-
-    let ringGeo = new THREE.RingGeometry(16, 20, 64)
-    let ringTextureLoad = new THREE.TextureLoader()
-    let ringTexture  = ringTextureLoad.load(
-        "./assets/textures/uranus_ring.png"
-    )
-
-    ringTexture.wrapS = THREE.RepeatWrapping
-    ringTexture.wrapT = THREE.RepeatWrapping
-
-    ringTexture.repeat.set(1, 1)
-    let ringMats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture,
-        side: THREE.DoubleSide
-    })
-    let ring= new THREE.Mesh(ringGeo, ringMats)
-    ring.castShadow = false
-    ring.receiveShadow = true
-    ring.rotation.x = Math.PI/-4
-
-    let uranusGroup = new THREE.Group()
-    uranusGroup.add(uranus)
-    uranusGroup.add(ring)
-    uranusGroup.position.set(280, 320, 0); // Set uranus's position as a group
-    return uranusGroup;
-}
-
-function createNeptune (){
-    let geo = new THREE.SphereGeometry(13)
-
-    let textureLoad = new THREE.TextureLoader()
-    let texture  = textureLoad.load(
-        "./assets/textures/neptune.jpg"
-    )
-
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
-
-    texture.repeat.set(1, 1)
-
-    let mats = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        map: texture
-    })
-    return new THREE.Mesh(geo, mats)
-}
-
 function createSatellite () {
     let geometry = new THREE.CylinderGeometry(1, 0.5, 0.4, 8);
 
@@ -396,15 +268,39 @@ function createSatellite () {
         roughness: 0.5,
     });
 
-    let satellite = new THREE.Mesh(geometry, material);
-    return satellite;
+    let satellite_obj = new THREE.Mesh(geometry, material);
+    satellite_obj.castShadow = false;
+    satellite_obj.receiveShadow = true;
+
+    return satellite_obj;
 };
+
+function updateSatellitePosition() {
+    if (earth && satellite) {
+        satellite.position.set(
+            earth.position.x + 8,
+            earth.position.y,
+            earth.position.z
+        );
+    }
+}
+
+function updateCameraPosition(){
+    camera.position.set(640, 480, 240)
+    camera.lookAt(640, 320, 0)
+
+    control.target.set(640, 320, 0);
+    control.update();
+}
+
+
 function render () {
     requestAnimationFrame(render)
     rndr.render(scene, selectedCamera)
     control.update()
     updateSpotlightPosition(model)
     updateCamera2Position(model)
+    updateSatellitePosition()
 }
 
 window.onresize = () => {
@@ -414,6 +310,7 @@ window.onresize = () => {
     camera.aspect = w/h
     rndr.setSize(w, h)
     camera.updateProjectionMatrix()
+
 }
 
 window.onload = () => {
